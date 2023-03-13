ca59cedbbc40eecec65731e0643653d5d0174f8a
RetroKoH-Base <cmgaudet2010@gmail.com>
2023-03-13 23:56:48 +0700

    Imported Sololearn Project (JS)

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+alert("This alert box was called with the onload event");
+
+let adults = 42;
+let children = 16;
+
+if (adults < 0) {
+    adults = 0;
+}
+
+if (children < 0) {
+    children = 0;
+}
