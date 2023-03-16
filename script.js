3324a1354c5d7b3cb8e5c92d2352c53a2cc04ce8
RetroKoH-Base <cmgaudet2010@gmail.com>
2023-03-16 14:32:12 +0700

    Added basic purchasing functionality

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // JavaScript source code
 //alert("This alert box was called with the onload event");
 
-let adults = 42;
-let children = 16;
-
-if (adults < 0) {
-    adults = 0;
+function calc(adults, children) {
+    return adults * 12 + children * 6;
 }
 
-if (children < 0) {
-    children = 0;
+window.onload = function() {
+    let btn = document.getElementById("buyBtn");
+    btn.onclick = function () {
+        let adults = document.getElementById("adults").value;
+        let children = document.getElementById("children").value;
+        let price = calc(adults, children);
+        alert(price);
+    }
 }
